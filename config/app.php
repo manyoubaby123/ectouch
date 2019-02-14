a0e9e33959fb201207b25abc87fc8d27f9b6cc97
wanganlin <wanganlin@ecmoban.com>
2019-02-14 15:02:40 +0800

    更新配置

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 
 return [
     'APP_SUB_DOMAIN_DEPLOY' => true,
-    'APP_SUB_DOMAIN_RULES' => ['admin' => 'Console'],
+    'APP_SUB_DOMAIN_RULES' => [ADMIN_PATH => 'Console'],
     'DEFAULT_MODULE' => 'Web',
     'MODULE_ALLOW_LIST' => ['Api', 'Web'],
 ];

6fb183ca42de968e315f6ed40836f62d0f56bc87
wanganlin <wanganlin@ecmoban.com>
2019-02-18 17:32:12 +0800

    更新路由

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,21 @@
 <?php
+
+Route::group('api', function () {
+
+    /**
+     * API version 3.0
+     */
+    Route::group('v3', function () {
+        Route::get('/', function () {
+            return 'home';
+        });
+
+    });
+
+    /**
+     * API Monitor
+     */
+    Route::get('monitor', function () {
+        return 'ok';
+    });
+});
